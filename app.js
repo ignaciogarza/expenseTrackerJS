8e24990288440da1a63a98f795fcaa3ea0c24bb6
Ignacio Garza <ignacioagarza@gmail.com>
2020-07-24 13:05:08 -0500

    added JS functionality to update the income and expense fields

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById("balance");
 const money_plus = document.getElementById("money-plus");
-const money_minus = document.getElementById("money-minu");
+const money_minus = document.getElementById("money-minus");
 const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
@@ -34,10 +34,34 @@ function addTransactionDOM(transactions) {
   list.appendChild(item);
 }
 
+// update balance income and expense
+
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+  console.log(expense);
+}
+
 // init app
 function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
